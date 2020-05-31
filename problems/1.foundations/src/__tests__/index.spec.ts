5620472eb42e8010677e39cd5674fb9a31ba7234
baoyx007 <baoyx007@gmail.com>
2020-05-31 14:27:16 +0800

    feat(p1): add chore

---
+++ problems/1.foundations/src/__tests__/index.spec.ts
@@ -0,0 +1,83 @@
+import { parseError } from '../index'
+const fixtureStack = `TypeError: Error raised
+  at bar http://192.168.31.8:8000/c.js:2:9
+  at foo http://192.168.31.8:8000/b.js:4:15
+  at calc http://192.168.31.8:8000/a.js:4:3
+  at <anonymous>:1:11
+  at http://192.168.31.8:8000/a.js:22:3
+`
+
+const fixtureFirefoxStack = `
+  bar@http://192.168.31.8:8000/c.js:2:9
+  foo@http://192.168.31.8:8000/b.js:4:15
+  calc@http://192.168.31.8:8000/a.js:4:3
+  <anonymous>:1:11
+  http://192.168.31.8:8000/a.js:22:3
+`
+describe('parseError', () => {
+  test('should chrome error works', () => {
+    let error = new Error('Error raised')
+    error.stack = fixtureStack
+
+    let errorMessage = parseError(error)
+
+    expect(errorMessage).toStrictEqual({
+      message: 'Error raised',
+      stack: [
+        {
+          line: 2,
+          column: 9,
+          filename: 'http://192.168.31.8:8000/c.js',
+        },
+        {
+          line: 4,
+          column: 15,
+          filename: 'http://192.168.31.8:8000/b.js',
+        },
+        {
+          line: 4,
+          column: 3,
+          filename: 'http://192.168.31.8:8000/a.js',
+        },
+        {
+          line: 22,
+          column: 3,
+          filename: 'http://192.168.31.8:8000/a.js',
+        },
+      ],
+    })
+  })
+
+  test('should firefoxs error works', () => {
+    let error = new Error('Error raised')
+    error.stack = fixtureFirefoxStack
+
+    let errorMessage = parseError(error)
+
+    expect(errorMessage).toStrictEqual({
+      message: 'Error raised',
+      stack: [
+        {
+          line: 2,
+          column: 9,
+          filename: 'http://192.168.31.8:8000/c.js',
+        },
+        {
+          line: 4,
+          column: 15,
+          filename: 'http://192.168.31.8:8000/b.js',
+        },
+        {
+          line: 4,
+          column: 3,
+          filename: 'http://192.168.31.8:8000/a.js',
+        },
+        {
+          line: 22,
+          column: 3,
+          filename: 'http://192.168.31.8:8000/a.js',
+        },
+      ],
+    })
+  })
+})
--- problems/1.foundations/src/index.ts
+++ problems/1.foundations/src/index.ts
@@ -9,4 +9,25 @@ export interface ErrorMessage {
 
 export function parseError(err: Error): ErrorMessage {
   // implement
+  // err.split("\n");
+  const { message } = err;
+  let stack = err.stack
+    ? err.stack
+        .split('\n')
+        .filter((s) => s.includes('http'))
+        .map((s) => {
+          const splitArr = s.split(':');
+          let filename = splitArr.slice(0, -2).join(':');
+          let [line, column] = splitArr.slice(-2);
+
+          filename = filename.slice(filename.indexOf('http'));
+
+          return {
+            filename,
+            line: parseInt(line, 10),
+            column: parseInt(column, 10),
+          };
+        })
+    : [];
+  return { message, stack };
 }
